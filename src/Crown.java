1251a51adeca28fc2cda46db2a7285771ee99846
Ivanst01 <susetsleeps@gmail.com>
2025-11-19 18:28:33 +0000

    initiated class Crown that returns value

---
+++ src/Crown.java
@@ -0,0 +1,26 @@
+/**
+ * A crown is a piece of headgear which value  is determined by its number of jewels
+ */
+
+
+public class Crown implements Headgear {
+
+    /**
+     * Constant value factor per jewel.
+     */
+    private static final int FIXED_VALUE_FACTOR = 200000;
+
+    /**
+     * Declares instant variable that stores the number of jewels in the crown.
+     */
+    private int numberOfJewels;
+
+
+
+    @Override
+    public double getValue() {
+        return numberOfJewels * FIXED_VALUE_FACTOR;
+    }
+
+
+}
