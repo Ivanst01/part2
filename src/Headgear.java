eebfef274759f88e9f9b51fa1bc668824a25b71b
Ivanst01 <susetsleeps@gmail.com>
2025-11-19 18:08:26 +0000

    Initial commit. Headgear interface implemented

---
+++ src/Headgear.java
@@ -0,0 +1,15 @@
+
+/**
+ * Public interface for all kinds od headgear.
+ * Each headgear computes its value in GBP.
+ */
+public interface Headgear {
+
+    /**
+     * Computes the value of this headgear in GBP
+     * 
+     * @return value in GBP
+     */
+
+    double getValue();
+}
